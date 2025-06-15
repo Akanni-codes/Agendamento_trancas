928c3bf75f8a4724e357a69c35e8aa2cdf8c3571
Akanni Silva <akanni029@gmail.com>
2025-06-15 12:21:06 -0300

    editando CorsConfig

--- src/main/java/com/agenda/trancas/config/CorsConfig.java
+++ src/main/java/com/agenda/trancas/config/CorsConfig.java
@@ -14,9 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins(
-                            "http://localhost:57683"   
-                        )
+                        .allowedOrigins("/*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
